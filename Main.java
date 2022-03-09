2b6ff6d6bf2a1c16c31bdbf484812090d1950b80
22prietor <64376102+22prietor@users.noreply.github.com>
2022-03-09 11:58:44 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,122 +1,57 @@
-package com.apptastic.rssreader;
-import java.io.*;
-import java.io.File;
-import java.io.PrintWriter;
-import java.lang.String;
-import java.util.Scanner;
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.List;
-import com.apptastic.rssreader.Channel;
-import com.apptastic.rssreader.DateTime;
-import com.apptastic.rssreader.Item;
-import com.apptastic.rssreader.RssReader;
-/*
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-*/
-import javax.net.ssl.SSLContext;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.time.Duration;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.concurrent.*;
-import java.util.function.Function;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-import java.util.zip.GZIPInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
-import static javax.xml.stream.XMLStreamConstants.CDATA;
-import static javax.xml.stream.XMLStreamConstants.CHARACTERS;
-import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
-import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+//import RSSLivestreaming.model.Feed;
+//import RSSLivestreaming.model.FeedMessage;
+//import RSSLivestreaming.write.RSSFeedWriter;
+
 class Main {
-  public static void main(String[] args) throws IOException {
-    System.out.println(java.time.LocalDateTime.now().toString());
-    RssReader reader = new RssReader();
-    Stream<Item> rssFeed = reader.read("https://511nj.org/RSS511Service/RSS511Service.svc/rest/rss/RSSActiveWeather");
-    File file = new File("data.txt");
-    PrintWriter pw = new PrintWriter(file);
-    pw.println(rssFeed);
-    pw.close();
-    Scanner scanner = new Scanner(file);
-    List<String> incidents = new ArrayList<String>();
-    scanner.useDelimiter(" -0500 ");
-    while (scanner.hasNext()) {
-      incidents.add(scanner.next() + " -0500 ");
-    }
+  public static void main(String[] args) {
+            // create the rss feed
+        String copyright = "511NJ";
+        String title = "RSS NJ Weather Data";
+        String description = "Current NJ Weather Information";
+        String language = "en";
+        String link = "https://511nj.org/RSS511Service/RSS511Service.svc/rest/rss/RSSActiveWeather";
+        Calendar cal = new GregorianCalendar();
+        Date creationDate = cal.getTime();
+        SimpleDateFormat date_format = new SimpleDateFormat("EEE', 'dd' 'MMM' 'yyyy' 'HH:mm:ss' 'Z", Locale.US);
+        String pubdate = date_format.format(creationDate);
+        Feed rssFeeder = new Feed(title, link, description, language, copyright, pubdate);
 
-    int elements = incidents.size() - 1;
-    String dates[] = new String[elements];
-    String times[] = new String[elements];
-    String events[] = new String[elements];
-    String localDate = java.time.LocalDate.now().toString();
-    String year = localDate.substring(0, localDate.indexOf("-"));
-    for (int i = 0; i < elements; i++) {
-      String s = String.valueOf(incidents.get(i));
-      int origin = 0;
-      for (int j = 0; j < s.length(); j++) {
-        if ((j == 0 || j == s.length() - 1) && (int) (s.charAt(j)) < 48 || (int) (s.charAt(j)) > 57) {
-          origin = j;
-          break;
+        // now add one example entry
+        /*
+        FeedMessage feed = new FeedMessage();
+        feed.setTitle("RSSFeed");
+        feed.setDescription("This is a description");
+        feed.setAuthor("nonsense@somewhere.de (Lars Vogel)");
+        feed.setGuid("https://www.vogella.com/tutorials/RSSFeed/article.html");
+        feed.setLink("https://511nj.org/RSS511Service/RSS511Service.svc/rest/rss/RSSActiveWeather");
+        rssFeeder.getMessages().add(feed);
+        */
+
+        // now write the file
+        RSSFeedWriter writer = new RSSFeedWriter(rssFeeder, "articles.rss");
+        try {
+            writer.write();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        if (j > 0 && j < s.length() - 1 && ((int) (s.charAt(j)) < 48 || (int) (s.charAt(j)) > 57)
-            && ((int) (s.charAt(j - 1)) < 48 || (int) (s.charAt(j - 1)) > 57)
-            && ((int) (s.charAt(j + 1)) < 48 || (int) (s.charAt(j + 1)) > 57)) {
-          origin = j;
-          break;
+        RSSFeedParser parser = new RSSFeedParser("https://511nj.org/RSS511Service/RSS511Service.svc/rest/rss/RSSActiveWeather");
+        Feed feed2 = parser.readFeed();
+        System.out.println(feed2.getCopyright());
+        System.out.println("All Active Flooding" + " " + feed2.getPubDate().substring(0, feed2.getPubDate().length() - 6));
+        for (FeedMessage message : feed2.getMessages()) {
+          if(message.getTitle().contains("Flood")) {
+          System.out.println("");
+          System.out.println("Event: " + message.getTitle());
+          System.out.println("Description: " + message.getDescription());
+            }
         }
-      }
-      int index = 0;
-      if (s.indexOf("Sun,") != -1) {
-        index = s.indexOf("Sun,");
-      }
-      if (s.indexOf("Mon,") != -1) {
-        index = s.indexOf("Mon,");
-      }
-      if (s.indexOf("Tues,") != -1) {
-        index = s.indexOf("Tues,");
-      }
-      if (s.indexOf("Wed,") != -1) {
-        index = s.indexOf("Wed,");
-      }
-      if (s.indexOf("Thu,") != -1) {
-        index = s.indexOf("Thu,");
-      }
-      if (s.indexOf("Fri,") != -1) {
-        index = s.indexOf("Fri,");
-      }
-      if (s.indexOf("Sat,") != -1) {
-        index = s.indexOf("Sat,");
-      }
-      dates[i] = s.substring(index, s.lastIndexOf(year) + 4);
-      times[i] = s.substring(s.lastIndexOf(year) + 5, s.indexOf(" -0500 "));
-      events[i] = s.substring(origin, index - 1);
-      if (s.toLowerCase().indexOf("flood") != -1) {
-        System.out.println("Date: " + dates[i]);
-        System.out.println("Time: " + times[i]);
-        System.out.println("Event: " + events[i]);
-        System.out.println(" ");
-      }
-    }
+
+        
   }
 }
